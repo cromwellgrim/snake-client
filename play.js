0e7969800f5c613093248110edd9b29f92e388fc
cromwellgrim <rogers.dylan@protonmail.com>
2021-02-10 22:05:11 +0000

    updated play with inputs

--- play.js
+++ play.js
@@ -1,4 +1,26 @@
 const connect = require('./client.js');
 
+function handleUserInput() {
+  const stdin = process.stdin;
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    };
+  });
+}
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  
+  handleUserInput();
+ 
+  return stdin;
+};
+
 console.log('Connecting ... ');
 connect();
+setupInput();

fc202d281985653e10ea38e6c81faef7605b16eb
cromwellgrim <rogers.dylan@protonmail.com>
2021-02-10 20:52:46 +0000

    updated play and created client

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require('net');
+
+const connect = function() {
+
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  conn.on ('connect', () => {
+    console.log('Server says: you connected');
+    conn.write('Name: RAD');
+  })
+
+  return conn;
+
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,20 +1,4 @@
-const net = require('net');
-
-const connect = function() {
-
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  conn.setEncoding('utf8');
-
-  conn.on('idleboot', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  return conn;
-}
+const connect = require('./client.js');
 
 console.log('Connecting ... ');
 connect();
